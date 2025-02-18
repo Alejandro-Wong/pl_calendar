af0ca771fdc0e1a23d89615b4cd47c39e3a3b511
Alejandro_Wong <121511772+Alejandro-Wong@users.noreply.github.com>
2025-02-18 15:47:54 -0500

    grayed out non-day boxes, profit/loss number color matches box color

--- src/Components/Day/Day.js
+++ src/Components/Day/Day.js
@@ -4,12 +4,14 @@ function Day({ index, dayNum, date, currentDay, plData}) {
   return (
     <div className="day"
         style={{border: currentDay === date ? '6px solid yellow;' : '1px solid darkgray',
-                "background-color": plData > 10 ? 'rgba(0, 255, 0, 0.2)' 
-                                  : plData < -10 ? 'rgba(255, 0, 0, 0.2)' : ''}}>
+                "background-color": !dayNum ? 'gray' 
+                                  : plData > 10 ? 'rgba(0, 255, 0, 0.2)' 
+                                  : plData < -10 ? 'rgba(255, 0, 0, 0.2)' : '', 
+                opacity: !dayNum ? 0.55 : 1}}>
       <div className="index">{index}</div>
       <div className="dayNum">{dayNum}</div>
       <div className="date">{date}</div>
-      <div className="plData">{plData ? plData : ''}</div>
+      <div className="plData" style={{color: plData > 0 ? '#0FFF50' : '#FF3131'}}>{plData ? plData : ''}</div>
     </div>
   )
 }
